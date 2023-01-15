302d2fadb2460f9923195f47880536c42e1d319e
John Ajera <jajera@fabrikam.local>
2023-01-15 14:35:00 +0800

    initial commit

--- Get-ServiceStatus.ps1
+++ Get-ServiceStatus.ps1
@@ -8,7 +8,7 @@ $redColor = "#FF0000"
 $orangeColor = "#FBB917" 
 $whiteColor = "#00FF00"
 
-$serviceList =  Get-Content .\servicelist.json | Out-String | ConvertFrom-Json
+$serviceList = Get-Content .\servicelist.json | Out-String | ConvertFrom-Json
 
 If (Test-Path $hcReport) { Remove-Item $hcReport } 
  
@@ -74,24 +74,22 @@ $tableHeader = "
 	" 
 Add-Content $hcReport $tableHeader 
 
-foreach($server in $serviceList) {
+foreach ($server in $serviceList) {
 	$computer = $server.name.toupper();
-	foreach($service in $server.services) {
+	foreach ($service in $server.services) {
 
-		$service_info = Get-Service -ComputerName $computer -ErrorAction SilentlyContinue | Where-Object {$_.DisplayName -eq $service.name } | Select-Object DisplayName, Status, StartType;
+		$service_info = Get-Service -ComputerName $computer -ErrorAction SilentlyContinue | Where-Object { $_.DisplayName -eq $service.name } | Select-Object DisplayName, Status, StartType;
 		$color_status_svc = $whiteColor;
 		$color_startup_svc = $whiteColor;
 
-        if($service_info.Status -ne $service.status)
-		{ 
-            $color_status_svc = $redColor
-        } 
-        if($service_info.StartType -ne $service.startup)
-		{ 
-            $color_startup_svc = $redColor
-        } 
+		if ($service_info.Status -ne $service.status) { 
+			$color_status_svc = $redColor
+		} 
+		if ($service_info.StartType -ne $service.startup) { 
+			$color_startup_svc = $redColor
+		} 
 
-    	$dataRow = " 
+		$dataRow = " 
             <tr> 
             <td width='10%' bgcolor='#36304a'>$computer</td> 
             <td width='10%' bgcolor='#36304a' align='center'>$($service.name)</td> 
@@ -99,7 +97,7 @@ foreach($server in $serviceList) {
             <td width='10%' bgcolor=$($color_startup_svc) align='center'>$($service_info.StartType)</td> 
             </tr> 
     	" 
-        Add-Content $hcReport $dataRow; 
+		Add-Content $hcReport $dataRow; 
 	}
 }
 
@@ -112,66 +110,3 @@ $tableDescription = "
     </table>
 	" 
 Add-Content $hcReport $tableDescription
-
-
-
-#     for($i=0; $i -lt $services.Count; $i++) 
-# 	{          
-
-
-
-#     }
-
-
-
-# foreach($computer in $computers) 
-# {  
-# 	$disks = Get-WmiObject -ComputerName $computer -Class Win32_LogicalDisk -Filter "DriveType = 3" 
-# 	$computer = $computer.toupper() 
-# 	foreach($disk in $disks) 
-# 	{         
-# 		$deviceID = $disk.DeviceID; 
-# 		$volName = $disk.VolumeName; 
-# 		[float]$size = [Math]::Round($disk.Size, 2); 
-# 		[float]$freespace = [Math]::Round($disk.FreeSpace, 2);  
-# 		$percentFree = [Math]::Round(($freespace / $size) * 100, 2); 
-# 		$sizeGB = [Math]::Round($size / 1073741824, 2); 
-# 		$freeSpaceGB = [Math]::Round($freespace / 1073741824, 2); 
-# 		$usedSpaceGB = [Math]::Round($sizeGB - $freeSpaceGB, 2);
-# 		$color = $whiteColor; 
- 
-# 		if($percentFree -lt $percentWarning)       
-# 		{ 
-# 			$color = $orangeColor  
-
-# 			if($percentFree -lt $percentCritcal) 
-# 			{ 
-# 				$color = $redColor 
-# 			}         
-# 		} 
-# 		$dataRow = " 
-# 		<tr> 
-# 		<td width='10%' bgcolor='#36304a'>$computer</td> 
-# 		<td width='5%' bgcolor='#36304a' align='center'>$deviceID</td> 
-# 		<td width='15%' bgcolor='#36304a'>$volName</td> 
-# 		<td width='10%' align='center' bgcolor='#36304a'>$sizeGB</td> 
-# 		<td width='10%' align='center' bgcolor='#36304a'>$usedSpaceGB</td> 
-# 		<td width='10%' align='center' bgcolor='#36304a'>$freeSpaceGB</td> 
-# 		<td width='5%' bgcolor=`'$color`' align='center'><font color='#36304a'>$percentFree</font></td> 
-# 		</tr> 
-# 		" 
-# 		Add-Content $hcReport $dataRow; 
-# 	} 
-# } 
-
-# $tableDescription = " 
-# 	</table><br><table width='20%'> 
-#     <table width=30%>
-#     <tr bgcolor='White'> 
-# 	<td width='50%' align='center' bgcolor='#FBB917'>Warning less than 15% free space</td> 
-# 	<td width='50%' align='center' bgcolor='#FF0000'>Critical less than 10% free space</td> 
-#     </tr>
-#     </table>
-# 	" 
-# Add-Content $hcReport $tableDescription
-# Add-Content $hcReport "</body></html>" 
